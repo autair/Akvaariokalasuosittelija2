4931b0f4e10748fa28e2ba95e77e9d72640c2c29
Taira Aurora <autair@da5-cs-bk107-25.helsinki.fi>
2016-04-11 14:06:14 +0300

    paivitys

---
+++ Akvaariokalaprojekti/src/main/java/akvaariokalasuosittelija/akvaariokalaprojekti/MessageListener.java
@@ -0,0 +1,13 @@
+
+package akvaariokalasuosittelija.akvaariokalaprojekti;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MessageListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        System.out.println("Viesti vastaanotettu!");
+    }
+}
